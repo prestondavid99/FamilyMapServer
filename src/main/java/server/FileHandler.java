c088ded716807697ce1625f2715e057623aedea6
Preston Thomas <preston@gmail.com>
2022-03-02 23:45:39 -0700

    Commit Changes

--- src/main/java/server/FileHandler.java
+++ src/main/java/server/FileHandler.java
@@ -6,35 +6,32 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.nio.file.Files;
 
 public class FileHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
-//        boolean success = false;
-//        try {
-//            if (exchange.getRequestMethod().toLowerCase().equals("get")) {
-//                exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
-//                exchange.getResponseBody().close();
-//                success = true;
-//            }
-//
-//            if (!success) {
-//
-//            }
-//        }
-        String urlPath = exchange.getRequestURI().toString();
-        if ((urlPath == null) || urlPath.equals("/")) {
-            urlPath = "/index.html";
+        try {
+            String urlPath = exchange.getRequestURI().toString();
+            if ((urlPath == null) || urlPath.equals("/")) {
+                urlPath = "/index.html";
+            }
+
+            String filePath = "web" + urlPath;
+            File website = new File(filePath);
+            OutputStream responseBody = exchange.getResponseBody();
+            if (!website.exists()) {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
+                exchange.close();
+            }
+            Files.copy(website.toPath(), responseBody);
+            responseBody.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
 
-        String filePath = "web" + urlPath;
-        File website = new File(filePath);
-        if (website.exists()) {
-            // Throw 404 error?
-        }
-        OutputStream responseBody = exchange.getResponseBody(); // TODO : No idea what these actually do
-        Files.copy(website.toPath(), responseBody);
     }
 }
